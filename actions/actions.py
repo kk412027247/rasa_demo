d528c49dbc2755088d585eb76a7ab1c98686414b
TMD <kk412027247@gmail.com>
2021-03-24 20:33:21 +0800

    init

---
+++ actions/actions.py
@@ -0,0 +1,43 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+
+class ActionFacilitySearch(Action):
+    def name(self) -> Text:
+        return "action_facility_search"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        facility = tracker.get_slot("facility_type")
+        location = tracker.get_slot("location")
+        address = "3001 Hyde St. San Francisco3"
+        print(address)
+        dispatcher.utter_message("4Here is the address of the {}:{}".format(facility, location))
+        return [SlotSet("address", address)]
